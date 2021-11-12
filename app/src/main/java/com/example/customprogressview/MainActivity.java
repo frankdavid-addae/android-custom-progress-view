0cbbde3aa696bf532873d13bf122a92dfc46c2f2
Frank David Addae <frankdavid.addae@gmail.com>
2021-11-12 17:22:01 +0000

    changing colors

--- app/src/main/java/com/example/customprogressview/MainActivity.java
+++ app/src/main/java/com/example/customprogressview/MainActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressView progressView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressView = findViewById(R.id.progressView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        progressView.setProgressAndPercentage("92");
     }
 }
--- app/src/main/java/com/example/customprogressview/ProgressView.java
+++ app/src/main/java/com/example/customprogressview/ProgressView.java
@@ -1,6 +1,13 @@
 package com.example.customprogressview;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.RotateDrawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +21,7 @@ import androidx.annotation.Nullable;
 public class ProgressView extends FrameLayout {
     ProgressBar progressBar;
     TextView tvPercentage;
+    LayerDrawable layerDrawable;
 
     public ProgressView(@NonNull Context context) {
         super(context);
@@ -23,6 +31,16 @@ public class ProgressView extends FrameLayout {
     public ProgressView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressView);
+
+        int progress = typedArray.getInteger(R.styleable.ProgressView_progressAndText, 0);
+        setProgressAndPercentage(String.valueOf(progress));
+        int color = typedArray.getColor(R.styleable.ProgressView_progressBaseColor, getResources().getColor(R.color.black));
+        setProgressBaseColor(color);
+        int insideColor = typedArray.getColor(R.styleable.ProgressView_insideColor, getResources().getColor(R.color.black));
+        setInsideColor(insideColor);
+        int progressColor = typedArray.getColor(R.styleable.ProgressView_progressColor, getResources().getColor(R.color.black));
+        setProgressColor(progressColor);
     }
 
     public ProgressView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -34,5 +52,44 @@ public class ProgressView extends FrameLayout {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.progress_view, this);
         progressBar = view.findViewById(R.id.progressBar);
         tvPercentage = view.findViewById(R.id.tvPercentage);
+        layerDrawable = (LayerDrawable) progressBar.getProgressDrawable();
+    }
+
+    public void setPercentage(String percentage) {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, Integer.parseInt(percentage));
+        valueAnimator.setDuration(1700);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                tvPercentage.setText(valueAnimator.getAnimatedValue().toString());
+            }
+        });
+        valueAnimator.start();
+    }
+
+    public void setProgress(int progress) {
+        ObjectAnimator.ofInt(progressBar, "progress", progress)
+        .setDuration(1700)
+        .start();
+    }
+
+    public void setProgressAndPercentage(String progress) {
+        setPercentage(progress);
+        setProgress(Integer.parseInt(progress));
+    }
+
+    public void setProgressBaseColor(int color) {
+        GradientDrawable gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.progress_base_color);
+        gradientDrawable.setColor(color);
+    }
+
+    public void setInsideColor(int color) {
+        GradientDrawable gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.inside_color);
+        gradientDrawable.setColor(color);
+    }
+
+    public void setProgressColor(int color) {
+        RotateDrawable rotateDrawable = (RotateDrawable) layerDrawable.findDrawableByLayerId(R.id.progress_color);
+        rotateDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
     }
 }
